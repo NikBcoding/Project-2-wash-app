2c83cb9b05ee3f167f5ffa1cb88f721962f27343
Nik Bagramyan <nikbagramyan@Nik-Azbekyans-MacBook-Pro.local>
2019-11-30 20:02:46 -0800

    install middleware

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+
 var logger = require('morgan');
+var methodOverride = require('method-override');
+
+//load the env vars
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//create the Express app
 var app = express();
 
 // view engine setup
@@ -17,6 +25,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'ABCwash',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
